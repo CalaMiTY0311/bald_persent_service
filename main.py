9eb5c7c58d16be8470e3dff89bf58c21eae4fd30
CalaMiTY0311 <axelsilas12@naver.com>
2023-12-13 03:39:50 +0900

    finally all clear cors fucking idiot

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ import joblib
 
 app = FastAPI()
 
-app.add_middleware( CORSMiddleware, 
-                   allow_origins=["*"],
-                #    allow_origins=["https://man-vs-talmo-client-he3s4xddp-shinis-projects.vercel.app"
-                #                   ,"http://localhost:3000"],
-				   allow_methods=["*"], 
-				   allow_headers=["*"], )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def hello():
