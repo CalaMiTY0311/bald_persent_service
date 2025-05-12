f1924fe0991b55e574f762a0e31c91471208965b
CalaMiTY0311 <axelsilas12@naver.com>
2025-05-13 04:02:42 +0900

    jenkins test and test run

--- runServer.sh
+++ runServer.sh
@@ -1,17 +1,17 @@
-bash#!/bin/bash
-set -x  # 디버그 모드 활성화
-echo "===== Docker 확인 및 설치 ====="
-cd /home/ubuntu
-if ! docker --version 2>/dev/null; then
-    echo "Installing Docker..."
-    sudo apt-get update
-    sudo apt-get install -y docker.io
-    sudo systemctl start docker
-    sudo systemctl enable docker
-    sudo usermod -aG docker $USER
-    echo "Docker installed!"
-else
-    echo "Docker already exists"
-    docker --version
-fi
-echo "Process completed!"
+# bash#!/bin/bash
+# set -x  # 디버그 모드 활성화
+# echo "===== Docker 확인 및 설치 ====="
+# cd /home/ubuntu
+# if ! docker --version 2>/dev/null; then
+#     echo "Installing Docker..."
+#     sudo apt-get update
+#     sudo apt-get install -y docker.io
+#     sudo systemctl start docker
+#     sudo systemctl enable docker
+#     sudo usermod -aG docker $USER
+#     echo "Docker installed!"
+# else
+#     echo "Docker already exists"
+#     docker --version
+# fi
+# echo "Process completed!"
