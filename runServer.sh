a8d88253f78597a52b7496ad95795d4609f068bc
CalaMiTY0311 <axelsilas12@naver.com>
2025-05-14 04:49:31 +0900

    jenkins test and test run

--- runServer.sh
+++ runServer.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 set -x
+
 echo "===== Docker 확인 및 설치 ====="
-cd /home/ubuntu || exit 1
 echo "Current directory: $(pwd)"
+
 if ! docker --version 2>/dev/null; then
     echo "Installing Docker..."
     sudo apt-get update || exit 1
     sudo apt-get install -y docker.io || exit 1
     sudo systemctl start docker || exit 1
     sudo systemctl enable docker || exit 1
-    sudo usermod -aG docker $USER || exit 1
     echo "Docker installed!"
 else
     echo "Docker already exists"
     docker --version
 fi
+
+if ! docker-compose --version 2>/dev/null; then
+    echo "Installing Docker Compose..."
+    sudo apt-get install -y docker-compose || exit 1
+    echo "Docker Compose installed!"
+else
+    echo "Docker Compose already exists"
+    docker-compose --version
+fi
+
 echo "Process completed!"
+
+sudo docker-compose down || true
+sudo docker-compose up -d
+sudo docker-compose ps
